13598103cbd82087164dc61a3feeb6bd81bbff9a
Gian <76099610+GianAlingog@users.noreply.github.com>
2025-02-23 12:58:18 -0500

    Add short template

---
+++ Template_Short.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+typedef vector<long long> vll;
+typedef vector<vector<long long>> vvll;
+typedef pair<int, int> pii;
+typedef pair<long long, long long> pll;
+typedef set<int> si;
+typedef set<long long> sll;
+
+const ll MOD = 998244353;
+const ld EPS = 1e-12;
+
+#define endl "\n"
+#define sp <<" "<<
+#define forn(i, n) for(ll i = 0; i < n; i++)
+#define rforn(i, n) for(ll i = n; i >= 0; i--)
+#define dbg(x) cout << #x << " = " << x << endl
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define INF 1e18
+#define fast_io() ios_base::sync_with_stdio(false); cin.tie(NULL)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) ((ll)(x).size())
+
+void solution() {
+    
+    return;
+}
+
+signed main() {
+    fast_io();
+
+    // solution();
+
+    int tests; cin >> tests;
+    while (tests--) {
+        solution();
+    }
+
+    return 0;
+}
