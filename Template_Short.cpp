4c83e4d7c39cc43877389f8f26c26cbccb2d3cd1
Gian <76099610+GianAlingog@users.noreply.github.com>
2025-03-19 18:43:11 +0800

    Update short template

--- Template_Short.cpp
+++ Template_Short.cpp
@@ -4,28 +4,19 @@ using namespace std;
 
 typedef long long ll;
 typedef long double ld;
-typedef vector<int> vi;
-typedef vector<vector<int>> vvi;
-typedef vector<long long> vll;
-typedef vector<vector<long long>> vvll;
-typedef pair<int, int> pii;
-typedef pair<long long, long long> pll;
-typedef set<int> si;
-typedef set<long long> sll;
 
 const ll MOD = 998244353;
+const ll INF = 1e18;
 const ld EPS = 1e-12;
 
 #define endl "\n"
 #define sp <<" "<<
-#define forn(i, n) for(ll i = 0; i < n; i++)
-#define rforn(i, n) for(ll i = n; i >= 0; i--)
-#define dbg(x) cout << #x << " = " << x << endl
+#define F0R(i, b) for(ll i = 0; i < (b); i++)
+#define FOR(i, a, b) for(ll i = (a); i < (b); i++)
 #define mp make_pair
 #define pb push_back
 #define fi first
 #define se second
-#define INF 1e18
 #define fast_io() ios_base::sync_with_stdio(false); cin.tie(NULL)
 #define all(x) (x).begin(), (x).end()
 #define rall(x) (x).rbegin(), (x).rend()
@@ -42,7 +33,7 @@ signed main() {
     // solution();
 
     int tests; cin >> tests;
-    while (tests--) {
+    while (tests --> 0) {
         solution();
     }
 
