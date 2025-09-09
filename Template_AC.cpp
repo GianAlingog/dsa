a6daf18428dc4ed0051ad229ebbf342c5137a9e0
Gian <76099610+GianAlingog@users.noreply.github.com>
2025-09-09 19:59:14 +0800

    Add Template_AC

---
+++ Template_AC.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+// #include <atcoder/all>
+using namespace std;
+// using namespace atcoder;
+// run task to compile local and subm for other
+
+typedef long long ll;
+typedef long double ld;
+
+#define sp <<" "<<
+#define endl "\n"
+
+void solve() {
+    
+}
+
+signed main() {
+    cin.tie(0);
+    ios_base::sync_with_stdio(false);
+
+    int t = 1;
+    // cin >> t;
+    while (t--)
+        solve();
+}
