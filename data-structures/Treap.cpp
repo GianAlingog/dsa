9933c3234860858c8b33eca8b625fe1795ffac1b
Gian <76099610+GianAlingog@users.noreply.github.com>
2025-10-14 20:32:46 -0700

    Update Treap

--- data-structures/Treap.cpp
+++ data-structures/Treap.cpp
@@ -4,148 +4,208 @@ using namespace std;
 typedef long long ll;
 typedef long double ld;
 
-#define sp <<" "<<
+const ll MOD = 998244353;
+const ll INF = 1e18;
+const ld EPS = 1e-12;
+
 #define endl "\n"
+#define sp <<" "<<
+#define REP(i, a, b) for(ll i = a; i < b; i++)
+#define fast_io() ios_base::sync_with_stdio(false); cin.tie(NULL)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+
+struct custom_hash {
+    static uint64_t splitmix64(uint64_t x) {
+        // http://xorshift.di.unimi.it/splitmix64.c
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+
+    size_t operator()(uint64_t x) const {
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+};
+
+template <typename Key, typename Value>
+using hash_map = unordered_map<Key, Value, custom_hash>;
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+// uniform_int_distribution<int>(a, b)(rng);
+// shuffle(all(a), rng);
 
 struct node {
-    ll val, prior, sum, sz;
+    ll key, prior, val, mn, sz;
     node *l, *r, *p;
-    node(ll val, ll prior) :
-        val(val), prior(prior), l(nullptr), r(nullptr), p(nullptr),
-        sz(1), sum(val)
+    node(ll k, ll v, ll prior) :
+        key(k), prior(prior), l(nullptr), r(nullptr), p(nullptr),
+        sz(1), val(v), mn(v)
     {;;}
 };
 
+ll getsz(node *t) { return t ? t->sz : 0; }
+ll getval(node *t) { return t ? t->mn : INT_MAX; }
+
 void pull(node *t) {
     if (!t) return;
-    t->sz = 1 + (t->l ? t->l->sz : 0) + (t->r ? t->r->sz : 0);
-    t->sum = t->val + (t->l ? t->l->sum : 0) + (t->r ? t->r->sum : 0);
-    if (t->l) t->l->p = t;
-    if (t->r) t->r->p = t;
+    t->sz = 1 + getsz(t->l) + getsz(t->r);
+    t->mn = min({t->val, getval(t->l), getval(t->r)});
 }
 
-pair<node*, node*> split(node *t, int k) {
-    if (!t) return {nullptr, nullptr};
-    if ((t->l ? t->l->sz : 0) >= k) {
-        auto res = split(t->l, k);
-        t->l = res.second;
-        if (t->l) t->l->p = t;
-        pull(t);
-        if (res.first) res.first->p = nullptr;
-        return {res.first, t};
+inline bool cmp(ll key1, ll val1, ll key2, ll val2) {
+    if (key1 == key2) return val1 < val2;
+    return key1 < key2;
+}
+
+void split(node *t, ll k, ll v, node *&a, node *&b) {
+    if (!t) { a = b = nullptr; return; }
+    if (cmp(t->key, t->val, k, v)) {
+        split(t->r, k, v, t->r, b);
+        a = t; pull(a);
     } else {
-        auto res = split(t->r, k - (t->l ? t->l->sz : 0) - 1);
-        t->r = res.first;
-        if (t->r) t->r->p = t;
-        pull(t);
-        if (res.second) res.second->p = nullptr;
-        return {t, res.second};
+        split(t->l, k, v, a, t->l);
+        b = t; pull(b);
     }
 }
 
 node* merge(node *a, node *b) {
-    if (!a or !b) {
-        node *t = a ? a : b;
-        if (t) t->p = nullptr;
-        return t;
-    }
-
+    if (!a or !b) return a ? a : b;
     if (a->prior < b->prior) {
-        a->r = merge(a->r, b);
-        if (a->r) a->r->p = a;
-        pull(a);
-        a->p = nullptr;
-        return a;
+        a->r = merge(a->r, b); pull(a); return a;
     } else {
-        b->l = merge(a, b->l);
-        if (b->l) b->l->p = b;
-        pull(b);
-        b->p = nullptr;
-        return b;
+        b->l = merge(a, b->l); pull(b); return b;
     }
 }
 
-int find(node *t) {
-    int res = (t->l ? t->l->sz : 0);
-    while (t->p) {
-        if (t->p->r == t) res += (t->p->l ? t->p->l->sz : 0) + 1;
-        t = t->p;
+node* insert(node *t, node *it) {
+    if (!t) return it;
+    if (it->prior < t->prior) {
+        split(t, it->key, it->val, it->l, it->r);
+        pull(it); return it;
     }
-    return res;
+    if (cmp(it->key, it->val, t->key, t->val)) t->l = insert(t->l, it);
+    else t->r = insert(t->r, it);
+    pull(t); return t;
 }
 
-void del(node *t, vector<node*> &pos) {
-    if (!t) return;
-
-    vector<node*> st;
-    st.push_back(t);
-
-    while (!st.empty()) {
-        node *u = st.back(); st.pop_back();
-        if (u->l) st.push_back(u->l);
-        if (u->r) st.push_back(u->r);
-        pos[u->val] = nullptr;
-        delete u;
+node* erase(node *t, ll k, ll v) {
+    if (!t) return nullptr;
+    if (t->key == k and t->val == v) {
+        node *res = merge(t->l, t->r);
+        delete t; return res;
     }
+    if (cmp(k, v, t->key, t->val)) t->l = erase(t->l, k, v);
+    else t->r = erase(t->r, k, v);
+    pull(t); return t;
 }
 
-mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+const int MX = 4e6;
+const int N = MX + 21;
+int at = 0;
+node* root;
+// map<pair<int, int>, int> fin;
+set<pair<int, int>> st; // [L, R]
+ 
+void update(pair<int, int> &seg, bool on) {
+    int w = seg.second - seg.first;
+    pair<int, int> temp{w, seg.first};
 
-void solve() {
+    if (on) {
+        root = insert(root, new node(w, seg.first, rng()));
+    } else {
+        root = erase(root, w, seg.first);
+    }
+}
+ 
+void reset() {
+    st.clear();
+    root = nullptr;
+    pair<int, int> temp{0, MX};
+    st.insert(temp);
+    update(temp, true);
+}
+ 
+void solution() {
+    reset();
+ 
+    auto add = [&](int x) -> void {
+        pair<int, int> temp{x, N};
+        auto it = st.upper_bound(temp);
+        it--;
+ 
+        pair<int, int> loc = *it;
+        if (loc.first == x) return;
+        pair<int, int> fir{loc.first, x-1}, sec{x, loc.second};
+ 
+        update(loc, false);
+        update(fir, true), update(sec, true);
+
+        st.erase(it);
+        st.insert(fir), st.insert(sec);
+    };
+ 
+    auto rem = [&](int x) -> void {
+        pair<int, int> temp{x, N};
+        auto it = st.upper_bound(temp);
+        it--;
+        
+        pair<int, int> sec = *it;
+        assert(sec.first == x);
+ 
+        pair<int, int> fir = *prev(it);
+ 
+        pair<int, int> loc = {fir.first, sec.second};
+ 
+        update(fir, false), update(sec, false);
+        update(loc, true);
+ 
+        st.erase(it);
+        it = st.find(fir);
+        st.erase(it);
+        st.insert(loc);
+    };
+ 
+    auto que = [&](int k) -> int {
+        node *a = nullptr, *b = nullptr;
+        split(root, k, INT_MIN, a, b);
+        int res = getval(b);
+        root = merge(a, b);
+        return res + 1;
+    };
+ 
+    int n; cin >> n;
+    while (n--) {
+        int x; cin >> x;
+        add(x);
+    }
+ 
     int q; cin >> q;
-
-    vector<node*> pos(q+5, nullptr);
-    node *root = new node(0, rng());
-    pos[0] = root;
-
-    for (int i = 1; i <= q; i++) {
-        int type; cin >> type;
-
-        if (type == 1) {
-            int x; cin >> x;
-            node *nx = pos[x];
-            int ind = find(nx);
-
-            auto res = split(root, ind + 1);
-            node *nw = new node(i, rng());
-            pos[i] = nw;
-            root = merge(merge(res.first, nw), res.second);
-        } else {
-            int x, y; cin >> x >> y;
-            node *nx = pos[x], *ny = pos[y];
-            int indx = find(nx), indy = find(ny);
-            if (indx > indy) swap(indx, indy);
-
-            int amt = indy - indx - 1;
-            if (amt <= 0) {
-                cout << 0 << endl;
-                continue;
-            }
-
-            auto r1 = split(root, indx + 1);
-            auto r2 = split(r1.second, amt);
-
-            cout << (r2.first ? r2.first->sum : 0) << endl;
-
-            del(r2.first, pos);
-            root = merge(r1.first, r2.second);
-        }
+    while (q--) {
+        char type; cin >> type;
+        int x; cin >> x;
+ 
+        if (type == '+') add(x);
+        else if (type == '-') rem(x);
+        else cout << que(x) << " ";
     }
-
-    // range deletion....
-    // feels like tree structure needed, BST
-    // we store the positions in the tree
-    // then deleting everything in range means removing a subtree
-    // i will totally pull a red black tree here
-    // just recalculate values after rebalancing
+    cout << endl;
+ 
+    // cerr << endl;
+    return;
 }
 
 signed main() {
-    cin.tie(0);
-    ios_base::sync_with_stdio(false);
+    fast_io();
 
-    int t = 1;
-    // cin >> t;
-    while (t--)
-        solve();
+    // solution();
+
+    int tests; cin >> tests;
+    while (tests --> 0) {
+        solution();
+    }
+
+    return 0;
 }
